ecf6b99fca7dce02fd556cb13f4ff4358b6f8969
ravindra-pandey <ravindra3885@gmail.com>
2023-10-15 15:44:25 +0530

    removed a.py

--- a.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from tensorflow.python.compiler.tensorrt import trt_convert as trt
-trt_model = trt.create_inference_graph(
-    input_graph_def=frozen_graph,
-    outputs=output_names,
-    max_batch_size=1,
-    max_workspace_size_bytes=(1 << 30),
-    precision_mode="FP16")
