84b43fe9bcb8872c28817f25d662744996bc40ae
ravindra-pandey <ravindra3885@gmail.com>
2023-10-15 14:59:39 +0530

    started severity model

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ uploaded_file = st.file_uploader(
 )
 
 model = tf.keras.models.load_model("models/binary_model.h5")
+severity_model=tf.keras.models.load_model("models/severity_model.h5")
+encoder=pickle.load(open("serialized_files/label_encoder.pkl","rb"))
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
@@ -28,6 +30,7 @@ if uploaded_file is not None:
     image_array = np.array(image)
 
     out = model.predict(np.array([image_array]))
+    severity = severity_model.predict(np.array([image_array]))
 
 col1, col2 = st.columns([0.55, 0.45])
 
@@ -38,6 +41,7 @@ with col1:
         st.header("your image will be shown here")
 with col2:
     bar2 = st.progress(0, text=f"Chances of infection :0 %")
+    st.write(f"level: {encoder.inverse_transform([np.argmax(severity)] )[0]}")
     if uploaded_file is not None:
         for i in range(int(out[0][1] * 100)):
             bar2.progress(i + 1, text=f"chances of infection : {out[0][1] * 100:.2f}%")
