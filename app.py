d1733c4377e51c6c57c1a6a5cac1d3cc99ee9a0c
ravindra-pandey <ravindra3885@gmail.com>
2023-10-15 15:40:59 +0530

    severity model on prgress

--- app.py
+++ app.py
@@ -13,8 +13,8 @@ uploaded_file = st.file_uploader(
 )
 
 model = tf.keras.models.load_model("models/binary_model.h5")
-severity_model=tf.keras.models.load_model("models/severity_model.h5")
-encoder=pickle.load(open("serialized_files/label_encoder.pkl","rb"))
+severity_model = tf.keras.models.load_model("models/severity_model.h5")
+encoder = pickle.load(open("serialized_files/label_encoder.pkl", "rb"))
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
@@ -40,8 +40,37 @@ with col1:
     else:
         st.header("your image will be shown here")
 with col2:
-    bar2 = st.progress(0, text=f"Chances of infection :0 %")
-    st.write(f"level: {encoder.inverse_transform([np.argmax(severity)] )[0]}")
+    bar1 = st.progress(0, text=f"Chances of infection :0 %")
+    st.write(f"level: {severity}")
+    bar2 = st.progress(0, text=encoder.inverse_transform([2])[0])
+    bar3 = st.progress(0, text=encoder.inverse_transform([0])[0])
+    bar4 = st.progress(0, text=encoder.inverse_transform([1])[0])
+    bar6 = st.progress(0, text=encoder.inverse_transform([4])[0])
+    bar5 = st.progress(0, text=encoder.inverse_transform([3])[0])
+
     if uploaded_file is not None:
         for i in range(int(out[0][1] * 100)):
-            bar2.progress(i + 1, text=f"chances of infection : {out[0][1] * 100:.2f}%")
+            bar1.progress(i + 1, text=f"chances of infection : {out[0][1] * 100:.2f}%")
+
+        for i in range(int(severity[0][2] * 100)):
+            bar2.progress(
+                i + 1, text=f"{encoder.inverse_transform([2])[0]}: {severity[0][2] * 100:.2f}%"
+            )
+
+        for i in range(int(severity[0][0] * 100)):
+            bar3.progress(
+                i + 1, text=f"{encoder.inverse_transform([0])[0]}: {severity[0][0] * 100:.2f}%"
+            )
+
+        for i in range(int(severity[0][1] * 100)):
+            bar4.progress(
+                i + 1, text=f"{encoder.inverse_transform([1])[0]}: {severity[0][1] * 100:.2f}%"
+            )
+
+        for i in range(int(severity[0][3] * 100)):
+            bar5.progress(
+                i + 1, text=f"{encoder.inverse_transform([3])[0]}: {severity[0][3] * 100:.2f}%"
+            )
+
+        for i in range(int(severity[0][4] * 100)):
+            bar6.progress(i + 1, text=f"{encoder.inverse_transform([4])[0]}: {severity[0][4] * 100:.2f}%")
