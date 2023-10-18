cebb50497b83d614eee1176e9dba140c458ddd79
ravindra-pandey <ravindra3885@gmail.com>
2023-10-18 16:38:38 +0530

    moved the data preprocessing module here

--- dataset_preprocessing.py
+++ dataset_preprocessing.py
@@ -1,4 +1,5 @@
-import os,datetime
+import os
+import zipfile
 from tqdm import tqdm
 
 import cv2
@@ -17,7 +18,33 @@ augmentor = alb.Compose([
     ])
 
 
+with zipfile.ZipFile("archive.zip", "r") as zip_ref:
+    zip_ref.extractall("dataset")
+    
+print("extraction done")
+
+print("data preprocessing started")
+
+image_names = [os.path.join(root, fl) for root, dirs, files in os.walk("dataset/colored_images/colored_images") for fl in files]
+
+for img_name in tqdm(image_names,desc="processing :"):
+  image=cv2.imread(img_name)
+  image=image[int(image.shape[0]*0.05):int(image.shape[0]*0.95),int(image.shape[1]*0.08):int(image.shape[1]*0.9)]
+  cv2.imwrite(img_name,image)
+
+
+os.makedirs("augmented_dataset/normal",exist_ok=True)
+os.makedirs("augmented_dataset/infected",exist_ok=True)
+for img_name in tqdm(image_names,desc="preprocessing images"):
+  for i in range(np.random.randint(1,3) if img_name.split('/')[-2]=='No_DR' else 2):
+    image=cv2.resize(cv2.imread(img_name),(224,224))
+    augmented=augmentor(image=image)["image"]
+    img_path=f"augmented_dataset/{'normal' if img_name.split('/')[-2]=='No_DR' else 'infected' }/{uuid1()}.jpeg"
+    cv2.imwrite(img_path,augmented)
+    
 image_names = [os.path.join(root, fl) for root, dirs, files in os.walk("dataset/colored_images/colored_images") for fl in files if "No_DR" not in root]
+
+
 class_count={}
 classes=os.listdir("dataset/colored_images/colored_images")
 classes.remove("No_DR")
