8d9c22f0cb71ba46676010bdc1ea8e78e19cfe03
ravindra-pandey <ravindra3885@gmail.com>
2023-10-18 16:03:21 +0530

    ...

---
+++ dataset_preprocessing.py
@@ -0,0 +1,34 @@
+import os,datetime
+from tqdm import tqdm
+
+import cv2
+import numpy as np
+from uuid import uuid1
+import albumentations as alb
+
+augmentor = alb.Compose([
+    alb.RandomCrop(width=224, height=224),
+    alb.HorizontalFlip(p=0.5),
+    alb.RandomBrightnessContrast(p=0.2),
+    alb.VerticalFlip(p=0.5),
+    alb.Rotate(limit=75, p=0.5),
+    alb.ISONoise(intensity=(0.05,0.15)),
+    alb.ZoomBlur()
+    ])
+
+
+image_names = [os.path.join(root, fl) for root, dirs, files in os.walk("dataset/colored_images/colored_images") for fl in files if "No_DR" not in root]
+class_count={}
+classes=os.listdir("dataset/colored_images/colored_images")
+classes.remove("No_DR")
+for i in classes:
+  class_count[i]=len(os.listdir(f"dataset/colored_images/colored_images/{i}"))
+  os.makedirs(f"severity_augmented_dataset/{i}",exist_ok=True)
+
+for img_name in tqdm(image_names, desc = "preprocessing images"):
+    aug_count=int(image_names.__len__()/class_count[img_name.split('/')[-2]])
+    for i in range(np.random.randint(1,3) if aug_count==1 else aug_count):
+      image = cv2.resize(cv2.imread(img_name),(224,224))
+      augmented=augmentor(image=image)["image"]
+      img_path=f"severity_augmented_dataset/{img_name.split('/')[-2]}/{uuid1()}.jpeg"
+      cv2.imwrite(img_path,augmented)
---
+++ download_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Check if the URL argument is provided
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <URL>"
+  exit 1
+fi
+
+# Get the URL argument
+url="$1"
+
+# Extract the file name from the URL
+output_file="archive.zip"
+
+# Download the file using curl
+curl -o "$output_file" "$url"
